b121f1afdd54f0ca3f4d717c670ad94fff82a987
osawa-koki <github@osawa.dev>
2023-02-28 21:46:36 +0900

    小さな修正。

--- setting.ts
+++ setting.ts
@@ -4,7 +4,7 @@ const Env = require('./next.config.js');
 const isProd = process.env.NODE_ENV === 'production';
 
 const Setting = {
-  title: '🐟 nextjs-template.modern 🐟',
+  title: '🌐 Learning Network 🌐',
   isProd,
   basePath: Env.basePath,
   apiPath: isProd ? '' : 'http://localhost:80',
