b596b67ed50a5c787d0fd1fdf077e2b07ec71187
osawa-koki <github@osawa.dev>
2023-03-02 15:23:55 +0900

    IPv4アドレスの検証関数のテスト。

---
+++ tests/isValidIPv4.test.ts
@@ -0,0 +1,15 @@
+export {};
+
+import isValidIPv4 from '../util/isValidIPv4';
+
+test("IPv4アドレスの検証関数のテスト", () => {
+  expect(isValidIPv4('10.0.0.0')).toBe(true);
+  expect(isValidIPv4('10.10.10.10')).toBe(true);
+  expect(isValidIPv4('255.255.255.255')).toBe(true);
+  
+  expect(isValidIPv4('255.255.255.256')).toBe(false);
+  expect(isValidIPv4('255.255.255.A')).toBe(false);
+  expect(isValidIPv4('255.255.255')).toBe(false);
+  expect(isValidIPv4('255.255.255.')).toBe(false);
+  expect(isValidIPv4('255.255.255.255.255')).toBe(false);
+});
