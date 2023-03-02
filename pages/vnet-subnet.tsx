2397b323224a56c27ac269ec08812e00854f4ade
osawa-koki <github@osawa.dev>
2023-03-02 11:35:24 +0900

    サブネットの入力まで。

--- pages/vnet-subnet.tsx
+++ pages/vnet-subnet.tsx
@@ -3,14 +3,29 @@ import React, { useEffect, useState } from "react";
 import { Alert, Form, Spinner, Table } from 'react-bootstrap';
 import Layout from "../components/Layout";
 
-
+const subnet_ids = 'ABCDE'.split('');
 
 export default function VNetSubnetPage() {
 
   const [vnet_ip, setVNetIP] = useState<string>('10.0.0.0');
   const [vnet_prefix, setVNetPrefix] = useState<string>('16');
-  const [subnet_ips, setSubnetIPs] = useState<string[]>([]);
-  const [subnet_prefixes, setSubnetPrefixes] = useState<string[]>([]);
+  const [subnets, setSubnets] = useState<{id: string, ip: string, prefix: string}[]>([
+    {id: 'A', ip: '10.0.1.0', prefix: '24'},
+    {id: 'B', ip: '10.0.2.0', prefix: '24'},
+  ]);
+
+  const PutSubnet = (id: string, e: any, type: 'vnet' | 'subnet') => {
+    const value = (e.target as HTMLInputElement).value;
+    const subnet = subnets.find((s) => s.id === id);
+    if (subnet) {
+      if (type === 'vnet') {
+        subnet.ip = value;
+      } else if (type === 'subnet') {
+        subnet.prefix = value;
+      }
+      setSubnets([...subnets]);
+    }
+  };
 
   return (
     <Layout>
@@ -29,6 +44,28 @@ export default function VNetSubnetPage() {
           </Form.Group>
         </Form>
         <hr />
+        <Table striped bordered hover size="sm" className="mt-3">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>サブネットIP</th>
+              <th>サブネットプレフィックス</th>
+            </tr>
+          </thead>
+          <tbody>
+            {subnets.map((subnet, _) => (
+              <tr key={subnet.id}>
+                <td>{subnet.id}</td>
+                <td>
+                  <Form.Control type="text" placeholder='10.0.0.0' value={subnet.ip} onInput={(e) => {PutSubnet(subnet.id, e, 'vnet')}} className='ip' />
+                </td>
+                <td>
+                  <Form.Control type="number" placeholder='24' value={subnet.prefix} onInput={(e) => {PutSubnet(subnet.id, e, 'subnet')}} className='prefix' />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
     </Layout>
   );
