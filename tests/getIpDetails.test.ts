1b4828eeda8832697d7c6cec34d9e1be9474e071
osawa-koki <github@osawa.dev>
2023-03-02 14:36:09 +0900

    getIpDetailsのテスト。

---
+++ tests/getIpDetails.test.ts
@@ -0,0 +1,27 @@
+export {};
+
+import getIpDetails from '../util/getIpDetails';
+
+test("'10.0.0.0/16'のテスト", () => {
+  const result = getIpDetails('10.0.0.0', 16);
+  expect(result.networkAddress).toBe('10.0.0.0');
+  expect(result.broadcastAddress).toBe('10.0.255.255');
+  expect(result.ipAddressStart).toBe('10.0.0.1');
+  expect(result.ipAddressEnd).toBe('10.0.255.254');
+});
+
+test("'10.0.0.0/24'のテスト", () => {
+  const result = getIpDetails('10.0.0.0', 24);
+  expect(result.networkAddress).toBe('10.0.0.0');
+  expect(result.broadcastAddress).toBe('10.0.0.255');
+  expect(result.ipAddressStart).toBe('10.0.0.1');
+  expect(result.ipAddressEnd).toBe('10.0.0.254');
+});
+
+test("'10.0.1.0/16'のテスト", () => {
+  const result = getIpDetails('10.0.0.0', 16);
+  expect(result.networkAddress).toBe('10.0.0.0');
+  expect(result.broadcastAddress).toBe('10.0.255.255');
+  expect(result.ipAddressStart).toBe('10.0.0.1');
+  expect(result.ipAddressEnd).toBe('10.0.255.254');
+});
