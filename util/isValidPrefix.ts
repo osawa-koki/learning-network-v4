0356e9911f659dce4e998092ce51fa4bd6c54341
osawa-koki <github@osawa.dev>
2023-03-02 15:50:09 +0900

    小さな修正。

--- util/isValidPrefix.ts
+++ util/isValidPrefix.ts
@@ -1,7 +1,7 @@
 
-function isValidIPv4(prefix: string): boolean {
+function isValidPrefix(prefix: string): boolean {
   const num = parseInt(prefix);
   return !isNaN(num) && num >= 0 && num <= 32;
 }
 
-export default isValidIPv4;
+export default isValidPrefix;
