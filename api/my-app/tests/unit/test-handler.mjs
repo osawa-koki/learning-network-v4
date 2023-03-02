d7bbe7f8f2e886bf3181deb51abfe462555a2768
osawa-koki <github@osawa.dev>
2023-03-02 21:33:20 +0900

    小さな修正。

--- api/my-app/tests/unit/test-handler.mjs
+++ api/my-app/tests/unit/test-handler.mjs
@@ -3,29 +3,10 @@
 import { lambdaHandler } from '../../app.mjs';
 import { expect } from 'chai';
 
-describe('Tests index', function () {
-  it('verifies successful response', async () => {
-
-    // const event = {
-    //   "requestContext": {
-    //     "identity": {
-    //       "sourceIp": "10.0.0.0"
-    //     }
-    //   }
-    // };
-    // const context = {};
-
-    // const result = await lambdaHandler(event, context)
-
-    // expect(result).to.be.an('object');
-    // expect(result.statusCode).to.equal(200);
-    // expect(result.body).to.be.an('string');
-
-    // let response = JSON.parse(result.body);
-
-    // expect(response).to.be.an('object');
-    // expect(response.message).to.be.equal("private range");
+describe('Tests Lambda', function () {
+  it('Dummy', async () => {
 
     expect(true).to.be.true;
+
   });
 });
