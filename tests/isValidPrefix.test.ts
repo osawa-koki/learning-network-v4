eb3cb0885b765f12042ca922ec98aef8570cf8b1
osawa-koki <github@osawa.dev>
2023-03-02 15:51:27 +0900

    プレフィックスの検証関数のテスト。

---
+++ tests/isValidPrefix.test.ts
@@ -0,0 +1,14 @@
+export {};
+
+import isValidPrefix from '../util/isValidPrefix';
+
+test('プレフィックスの検証関数のテスト', () => {
+  expect(isValidPrefix('0')).toBe(true);
+  expect(isValidPrefix('8')).toBe(true);
+  expect(isValidPrefix('16')).toBe(true);
+  expect(isValidPrefix('32')).toBe(true);
+  
+  expect(isValidPrefix('-1')).toBe(false);
+  expect(isValidPrefix('33')).toBe(false);
+  expect(isValidPrefix('foo')).toBe(false);
+});
