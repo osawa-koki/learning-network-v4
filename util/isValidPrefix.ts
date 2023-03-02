c9af00315fdd9125f0c22678d165dbc90f48516c
osawa-koki <github@osawa.dev>
2023-03-02 23:03:31 +0900

    プレフィックスで小数を禁止に。

--- util/isValidPrefix.ts
+++ util/isValidPrefix.ts
@@ -1,6 +1,9 @@
 
 function isValidPrefix(prefix: string): boolean {
   const num = parseInt(prefix);
+  if (num !== parseFloat(prefix)) {
+    return false; // 少数はNG
+  }
   return !isNaN(num) && num >= 0 && num <= 32;
 }
 
