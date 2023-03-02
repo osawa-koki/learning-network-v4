1b80507a7fade07c5e2d3ab9620b095d976b7a45
osawa-koki <github@osawa.dev>
2023-03-02 17:21:10 +0900

    小さな修正。

--- util/collisionChecker.ts
+++ util/collisionChecker.ts
@@ -10,8 +10,8 @@ function collisionChecker(subnet: SubnetStruct, other_subnets: SubnetStruct[]): 
   const subnet_ip = subnet.ip;
   const subnet_prefix = subnet.prefix;
   const subnet_bits = getIPAddressBits(subnet_ip);
-  const subnet_bits_prefix = subnet_bits.substr(0, Number(subnet_prefix));
-  const subnet_network_bits = subnet_bits_prefix.padEnd(32, '0');
+  const subnet_mask_bits = '1'.repeat(Number(subnet_prefix)).padEnd(32, '0');
+  const subnet_network_bits = (parseInt(subnet_bits, 2) & parseInt(subnet_mask_bits, 2)).toString(2).padStart(32, '0');
 
   const collision_subnet_ids: string[] = [];
 
@@ -19,8 +19,9 @@ function collisionChecker(subnet: SubnetStruct, other_subnets: SubnetStruct[]): 
     const other_subnet_ip = other_subnet.ip;
     const other_subnet_prefix = other_subnet.prefix;
     const other_subnet_bits = getIPAddressBits(other_subnet_ip);
-    const other_subnet_bits_prefix = other_subnet_bits.substr(0, Number(other_subnet_prefix));
-    const other_subnet_network_bits = other_subnet_bits_prefix.padEnd(32, '0');
+    const other_subnet_mask_bits = '1'.repeat(Number(other_subnet_prefix)).padEnd(32, '0');
+    const other_subnet_network_bits = (parseInt(other_subnet_bits, 2) & parseInt(other_subnet_mask_bits, 2)).toString(2).padStart(32, '0');
+
     if (subnet_network_bits === other_subnet_network_bits && subnet_prefix === other_subnet_prefix && subnet_ip !== other_subnet_ip) {
       collision_subnet_ids.push(other_subnet.id);
     }
