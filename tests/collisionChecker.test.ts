c466417ce9de7cd00286510d1938a0846a7fc72d
osawa-koki <github@osawa.dev>
2023-03-02 17:10:56 +0900

    衝突検知関数のテスト。

---
+++ tests/collisionChecker.test.ts
@@ -0,0 +1,49 @@
+export {};
+
+import collisionChecker, { SubnetStruct } from "../util/collisionChecker";
+
+test("衝突検知関数のテスト", () => {
+  {
+    const subnet: SubnetStruct = {
+      id: "1",
+      ip: "10.0.0.0",
+      prefix: "24",
+    };
+    const other_subnets: SubnetStruct[] = [
+      {
+        id: "2",
+        ip: "10.0.1.0",
+        prefix: "24",
+      },
+      {
+        id: "3",
+        ip: "10.0.2.0",
+        prefix: "24",
+      },
+    ];
+    const [collision, collision_subnet_ids] = collisionChecker(subnet, other_subnets);
+    expect(collision).toBe(false);
+    expect(collision_subnet_ids.length).toEqual(0);
+  }
+  {
+    const subnet: SubnetStruct = {
+      id: "1",
+      ip: "10.0.0.0",
+      prefix: "24",
+    };
+    const other_subnets: SubnetStruct[] = [
+      {
+        id: "2",
+        ip: "10.0.1.0",
+        prefix: "24",
+      },
+      {
+        id: "3",
+        ip: "10.0.1.0",
+        prefix: "24",
+      },
+    ];
+    const [collision, collision_subnet_ids] = collisionChecker(subnet, other_subnets);
+    expect(collision).toBe(true);
+  }
+});
