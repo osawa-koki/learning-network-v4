597312ebb6ac7054e5141ab73a6dbb95017b0e0b
osawa-koki <github@osawa.dev>
2023-03-03 00:42:51 +0900

    JSDocコメント追加した。

--- util/getNextSubnetIp.ts
+++ util/getNextSubnetIp.ts
@@ -1,6 +1,12 @@
 import getIpDetails from "./getIpDetails";
 import getIPAddressBits from "./getIPAddressBits";
 
+/**
+ * 次に使用できるサブネットのIPアドレスを取得する
+ * @param ip 最後のサブネットのIPアドレス
+ * @param prefix サブネットマスクのプレフィックス
+ * @returns 次に使用できるサブネットのIPアドレス
+ */
 function getNextSubnetIp(ip: string, prefix: string) {
   const { broadcastAddress } = getIpDetails(ip, parseInt(prefix));
   const broadcastAddressBits = getIPAddressBits(broadcastAddress);
@@ -9,6 +15,11 @@ function getNextSubnetIp(ip: string, prefix: string) {
   return nextSubnetIp;
 }
 
+/**
+ * 2進数の文字列を1つ加算する
+ * @param str 2進数の文字列
+ * @returns 加算した結果の2進数の文字列
+ */
 function addOneToBitString(str: string): string {
   const len = str.length;
   let carry = 1;
