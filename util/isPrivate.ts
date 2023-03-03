90926a4798ac13db57e34adb7f05ecd65f99d9a1
osawa-koki <github@osawa.dev>
2023-03-03 22:08:49 +0900

    isPrivate

---
+++ util/isPrivate.ts
@@ -0,0 +1,32 @@
+import isValidIPv4 from "./isValidIPv4";
+
+/**
+ * プライベートIPアドレスかどうかを判定する
+ * @param ipAddress IPアドレス
+ * @returns プライベートIPアドレスの場合は、'private-A' | 'private-B' | 'private-C'、それ以外は'public'を返す。不正なIPアドレスの場合はnullを返す。
+ */
+function isPrivate(ipAddress: string): 'public' | 'private-A' | 'private-B' | 'private-C' | null {
+  const ipArray = ipAddress.split(".");
+  const first = parseInt(ipArray[0], 10);
+  const second = parseInt(ipArray[1], 10);
+
+  if (isValidIPv4(ipAddress) === false) {
+    return null;
+  }
+
+  if (first === 10) {
+    return 'private-A';
+  }
+
+  if (first === 172 && second >= 16 && second <= 31) {
+    return 'private-B';
+  }
+
+  if (first === 192 && second === 168) {
+    return 'private-C';
+  }
+
+  return 'public';
+}
+
+export default isPrivate;
